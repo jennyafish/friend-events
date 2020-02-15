1c9c56a2a0148bc4c29bde00ef830ab7776926bb
Natalie <ronghu1202@gmail.com>
2020-02-15 16:23:12 -0500

    changed descriptions

--- friend-events-site/src/components/findevent.js
+++ friend-events-site/src/components/findevent.js
@@ -17,9 +17,10 @@ class FindEvent extends Component {
                     <Card shadow={10} style={{maxwidth: '600px', margin: 'auto'}}>
                         <CardTitle style={{color: '#fff', height: '176px', background: 'url(http://a2.espncdn.com/combiner/i?img=/photo/2014/0519/nfl_u_heinzts_1296x729.jpg) center / cover'}}>Steeler's Game</CardTitle>
                         <CardText>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                            Mauris sagittis pellentesque lacus eleifend lacinia...
+                             Catch a Steeler's game at Ford Field <br/>
+                            Feb 16, 3pm EST
                         </CardText>
+
                         <CardActions border>
                             <Button colored>Join Event</Button>
                         </CardActions>
@@ -30,8 +31,8 @@ class FindEvent extends Component {
                         <CardTitle style={{color: '#fff', height: '176px', 
                         background: 'url(https://cdn.vox-cdn.com/thumbor/xp8GDdbiMCqNK7x7egYAxkOK--0=/0x0:4008x2652/1200x800/filters:focal%281621x518:2261x1158%29/cdn.vox-cdn.com/uploads/chorus_image/image/61971367/usa_today_11533554.0.jpg) center / cover'}}>UPitt Game</CardTitle>
                         <CardText>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                            Mauris sagittis pellentesque lacus eleifend lacinia...
+                            Catch a Panther's game at Ford Field <br/>
+                            Feb 14, 8pm EST
                         </CardText>
                         <CardActions border>
                             <Button colored>Join Event</Button>
@@ -45,8 +46,8 @@ class FindEvent extends Component {
                         <CardTitle style={{color: '#fff', height: '176px', 
                         background: 'url(https://assets.rbl.ms/14932190/980x.jpg) center / cover'}}>Ice Skating</CardTitle>
                         <CardText>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                            Mauris sagittis pellentesque lacus eleifend lacinia...
+                            Ice Skate at Market Square <br/>
+                            Feb 21, 6pm EST
                         </CardText>
                         <CardActions border>
                             <Button colored>Join Event</Button>
@@ -67,8 +68,9 @@ class FindEvent extends Component {
                         <CardTitle style={{color: '#fff', height: '176px', 
                         background: 'url(https://www.visitpittsburgh.com/imager/files_idssasp_com/public/C32/1529a01f-6df8-4770-973c-840f62ca26a8/82e4dd84-2143-45aa-a8bb-c2d173c26ece/a35306f0-2266-495c-bb67-5c73b24f52bf_06a96c204074c363c2b2ae6c5709a5a1.jpg) center / cover'}}>Alleggeny Outdoor Show</CardTitle>
                         <CardText>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                            Mauris sagittis pellentesque lacus eleifend lacinia...
+                            Watch people perform at the outdoor sports show! <br/>
+                            Feb 15, 3:30pm EST
+
                         </CardText>
                         <CardActions border>
                             <Button colored>Join Event</Button>
@@ -91,11 +93,12 @@ class FindEvent extends Component {
                         <CardTitle style={{color: '#fff', height: '176px', 
                         background: 'url(https://www.popcultureonline.net/wp-content/uploads/2013/08/Fall-Out-Boy-Concert-Singapore-01.jpg) center / cover'}}>Fall Out Boy</CardTitle>
                         <CardText>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                            Mauris sagittis pellentesque lacus eleifend lacinia...
+                            Concert! <br/>
+                            March 17, 8pm EST
                         </CardText>
                         <CardActions border>
                             <Button colored>Join Event</Button>
+
                         </CardActions>
                         <CardMenu style={{color: '#fff'}}>
                             
@@ -106,8 +109,8 @@ class FindEvent extends Component {
                         <CardTitle style={{color: '#fff', height: '176px', 
                         background: 'url(https://media.wmagazine.com/photos/59243691c711f04ad921f407/4:3/w_1536/GettyImages-633746418.jpg) center / cover'}}>Ariana Grande</CardTitle>
                         <CardText>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                            Mauris sagittis pellentesque lacus eleifend lacinia...
+                            Concert! <br/>
+                            March 21, 8:30pm EST
                         </CardText>
                         <CardActions border>
                             <Button colored>Join Event</Button>
@@ -121,8 +124,8 @@ class FindEvent extends Component {
                         <CardTitle style={{color: '#fff', height: '176px', 
                         background: 'url(https://i.pinimg.com/originals/af/6c/a4/af6ca455942324c9244ea1485dc4f496.jpg) center / cover'}}>Shawn Mendes</CardTitle>
                         <CardText>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                            Mauris sagittis pellentesque lacus eleifend lacinia...
+                            Concert! <br/>
+                            April 12 , 7pm EST
                         </CardText>
                         <CardActions border>
                             <Button colored>Join Event</Button>
@@ -200,8 +203,7 @@ class FindEvent extends Component {
                         <CardTitle style={{color: '#fff', height: '176px', 
                             background: 'url(https://www.dvdsreleasedates.com/posters/800/S/Star-Wars-Episode-VIII---The-Last-Jedi-2017-movie-poster.jpg) center / cover'}}>Last Jedi</CardTitle>
                         <CardText>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                            Mauris sagittis pellentesque lacus eleifend lacinia...
+                            Catch StarWars at AMC!
                         </CardText>
                         <CardActions border>
                             <Button colored>Join Event</Button>
@@ -215,9 +217,9 @@ class FindEvent extends Component {
                         <CardTitle style={{color: '#fff', height: '176px', 
                         background: 'url(https://i.ytimg.com/vi/isOGD_7hNIY/maxresdefault.jpg) center / cover'}}>Parasite</CardTitle>
                         <CardText>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                            Mauris sagittis pellentesque lacus eleifend lacinia...
+                           Watch Parasite at Star Theatre!
                         </CardText>
+
                         <CardActions border>
                             <Button colored>Join Event</Button>
                         </CardActions>
@@ -230,8 +232,7 @@ class FindEvent extends Component {
                         <CardTitle style={{color: '#fff', height: '176px', 
                         background: 'url(https://i.redd.it/2kca6d7hlbs31.jpg) center / cover'}}>Jumanji 2</CardTitle>
                         <CardText>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                            Mauris sagittis pellentesque lacus eleifend lacinia...
+                            Watch Jumanji 2 at The Civic!
                         </CardText>
                         <CardActions border>
                             <Button colored>Join Event</Button>
