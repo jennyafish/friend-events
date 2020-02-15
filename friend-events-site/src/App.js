ce53c08572282da1a00671ce1a61b8c8b261e2e7
Katherine Wang <kat@katherines-mbp.wifi.cmu.edu>
2020-02-14 22:39:50 -0500

    Added nav bar

--- friend-events-site/src/App.js
+++ friend-events-site/src/App.js
@@ -1,11 +1,30 @@
 import React, {Component} from 'react';
 import './App.css';
+import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
 
 class App extends Component {
   render() {
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div style={{height: '1000px', position: 'relative'}}>
+        <Layout style={{background: 'url(http://www.getmdl.io/assets/demos/transparent.jpg) center / cover'}}>
+            <Header transparent title="Title" style={{color: 'white'}}>
+                <Navigation>
+                    <a href="/">Link</a>
+                    <a href="/">Link</a>
+                    <a href="/">Link</a>
+                    <a href="/">Link</a>
+                </Navigation>
+            </Header>
+            <Drawer title="Title">
+                <Navigation>
+                    <a href="/">Link</a>
+                    <a href="/">Link</a>
+                    <a href="/">Link</a>
+                    <a href="/">Link</a>
+                </Navigation>
+            </Drawer>
+            <Content />
+        </Layout>
     </div>
   );
  }
