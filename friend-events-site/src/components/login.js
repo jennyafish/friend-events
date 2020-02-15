40a8e202d1cd073a2ef1e7ab52ad50ef0e252f91
Katherine Wang <kathylu.wang@gmail.com>
2020-02-15 15:18:45 -0500

    Added login form

--- friend-events-site/src/components/login.js
+++ friend-events-site/src/components/login.js
@@ -1,11 +1,34 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
+import { Grid, Cell } from 'react-mdl';
 
-class Login extends Component {
-    render() {
+function Login() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
         return(
-            <div><h1>Login Page</h1></div>
-        )
-    }
+            <div style={{width: '100%', margin: 'auto'}}>
+                <form className="form-layout">
+                    <fieldset>
+                    <legend>Login</legend>
+                    <p>
+                      <label>Email</label>
+                      <input type = "text"
+                             id = "myText"
+                             value = {email}
+                             placeholder = "example.gmail.com"
+                             onChange = {(event) => setEmail(event.target.value)} />
+                    </p>
+                    <p>
+                      <label>Password</label>
+                      <input type = "password"
+                              id = "myPwd"
+                              value = {password}
+                              onChange = {(event) => setPassword(event.target.value)} />
+                    </p>
+                    </fieldset>
+                </form>
+            </div>
+        );
 }
 
 export default Login;
