551e06314e0cf7b48116c97be16bbf1357b29ef2
Jenny Fish <jafish@andrew.cmu.edu>
2020-02-14 20:17:03 -0500

    set up server

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
 const express = require('express');
+const debug = require('debug')('http');
+const http = require('http');
+
 const app = express();
 
+debug('booting');
+
 app.get('/', function (req, res) {
-  res.send('Hello World');
+  debug('got to route /');
+  res.send('Homepage');
 })
 
-module.exports = app;
+
+const server = http.createServer(app);
+
+function onListening() {
+  var addr = server.address();
+  var bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+  debug('Listening on ' + bind);
+}
+
+server.listen('8000');
+server.on('listening', onListening);
